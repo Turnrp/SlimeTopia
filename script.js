40e213eb87c039a8a5f0b3b9563b786f76d872ba
Turnrp <zewynne5@gmail.com>
2025-03-07 04:07:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const canvas = document.createElement("canvas");
-    const gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-
-    if (gl) {
-        // Check for WebGL errors and reload on error
-        function checkWebGLErrors() {
-            const error = gl.getError();
-            if (error !== gl.NO_ERROR) {
-                location.reload();
-            }
-            requestAnimationFrame(checkWebGLErrors);
-        }
-        checkWebGLErrors();
-
-        // Close the page if WebGL context is lost
-        canvas.addEventListener("webglcontextlost", (event) => {
-            event.preventDefault();
-            window.close();
-        });
+var original = window.console
+window.console = {
+    error: function(){
+        location.reload();
     }
-});
+}
